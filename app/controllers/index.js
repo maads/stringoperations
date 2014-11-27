312a1681cd837e3e88cdc42138c12be6cd3be56b
Mads Tordal <mads.tordal@gmail.com>
2014-11-27 18:19:13 -0500

    showing most used character in string

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,5 +13,29 @@ export default Ember.ObjectController.extend({
       return this.get('inputstring').length - this.get('whitespace');
     }
     return 0;
-  }.property('inputstring', 'whitespace')
+  }.property('inputstring', 'whitespace'),
+  mostUsed: function(){
+    var wordCounts = { };
+    if(!Ember.isEmpty(this.get('inputstring'))){
+      var inputstring = this.get('inputstring') ;
+      var characters = {};
+      var mostUsedCharacter = '';
+      for(var i = 0; i < inputstring.length; i++)
+        {
+          var key = inputstring[i];
+          if(!characters[key]){
+            characters[key] = 0;
+          }
+          characters[key]++;
+          if(mostUsedCharacter == '' || characters[key] > characters[mostUsedCharacter]){
+            mostUsedCharacter = key;
+          }
+        }
+        var frequency = characters[mostUsedCharacter];
+        if(mostUsedCharacter === " ")
+          mostUsedCharacter = "whitespace";
+        return mostUsedCharacter + " (" + frequency  + ")";
+    }
+    return "";
+  }.property('inputstring')
 });
